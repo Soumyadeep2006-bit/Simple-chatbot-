5a9122d596b90dd05d9f47f5c7973f306485eae2
Soumyadeep <dassoumyadeep2006@gmail.com>
2026-01-09 00:00:33 +0530

    Second Commit

--- get_cursor.py
+++ get_cursor.py
@@ -4,7 +4,7 @@ import openai
 
 while True:
     a=pyautogui.position()
-    print(a)
+    print(a)S
     #chrome in taskbar x=652,y=1064
     #whatsapp window ,start 649,895 (new=635,155),end=650,1018
     #to deselect (1008,918)
